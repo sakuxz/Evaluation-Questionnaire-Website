349726afea72fdb698c2233acb6cb5e59a18b45f
OOR <yui12327@gmail.com>
2016-03-26 00:18:28 +0800

    add extend ques-form animation

--- assets/js/index.jsx
+++ assets/js/index.jsx
@@ -3,6 +3,43 @@ require('index.scss');
 const React = require('react');
 const ReactDOM = require('react-dom');
 
+var FloatingButton = React.createClass({
+  getInitialState: function() {
+    return {
+      open: false,
+      dasda:"jghjhgjh"
+    };
+  },
+  render: function() {
+    var buttonClass = "yellow circular ui icon button fab";
+    return (
+      <button id="question" onClick={this.openQue} className={(this.state.open)?buttonClass+" open":buttonClass}>
+        <div>
+          <div className="pop-up">填寫問卷</div>
+          <i className="edit icon"></i>
+        </div>
+        <QuestionForm />
+      </button>
+    );
+  },
+  openQue: function(e) {
+    e.stopPropagation();
+    this.setState({
+      open: true
+    });
+  },
+  componentDidMount: function() {
+    this.setState({
+      open: false
+    });
+    $("body").click(function(e) {
+      this.setState({
+        open: false
+      });
+    }.bind(this));
+  }
+});
+
 var QuestionForm = React.createClass({
   getInitialState: function() {
     return {
@@ -13,6 +50,7 @@ var QuestionForm = React.createClass({
   render: function() {
     return (
       <div className="ques-form">
+
         <div className="ui form">
           <h4 className="ui dividing header">基本資料</h4>
           <div className="fields">
@@ -26,22 +64,22 @@ var QuestionForm = React.createClass({
             </div>
           </div>
           <div className="inline fields">
-            <label for="fruit">Select your favorite fruit:</label>
+            <label>Select your favorite fruit:</label>
             <div className="field">
               <div className="ui radio checkbox">
-                <input type="radio" name="fruit" checked="" tabindex="0" className="hidden"/>
+                <input type="radio" name="fruit" className="hidden"/>
                 <label>Apples</label>
               </div>
             </div>
             <div className="field">
               <div className="ui radio checkbox">
-                <input type="radio" name="fruit" tabindex="0" className="hidden"/>
+                <input type="radio" name="fruit" className="hidden"/>
                 <label>Oranges</label>
               </div>
             </div>
             <div className="field">
               <div className="ui radio checkbox">
-                <input type="radio" name="fruit" tabindex="0" className="hidden"/>
+                <input type="radio" name="fruit" className="hidden"/>
                 <label>Pears</label>
               </div>
             </div>
@@ -59,4 +97,4 @@ var QuestionForm = React.createClass({
 });
 
 ReactDOM.render(
-  <QuestionForm/>, document.querySelector(".content-wrapper"));
+  <FloatingButton/>, document.querySelector(".content-wrapper"));
