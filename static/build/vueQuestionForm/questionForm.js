a1cd8454aa62a0d04346db7bac03b4242c7cd975
sakuxz <yui12327@gmail.com>
2017-01-10 03:58:21 +0800

    modify situation-order method

--- static/build/vueQuestionForm/questionForm.js
+++ static/build/vueQuestionForm/questionForm.js
@@ -19,37 +19,31 @@ Vue.component('question-form', {
   }
 })
 
+var situationInfo = {
+  angry: '讓人失望透頂 CP 值極低的下午茶 ♥Destino 你是我的命運餐廳',
+  worry: '踩到地雷【Pachamama】團購卷 (怎麼辦?! 還有 4 張卷沒用完)',
+  pride: '【福鼎湯包店】有做功課有差，果然好吃 (得意) ！',
+  surprise: '不起眼、卻超出想像好吃的餐廳!!',
+}
 
 var app = new Vue({
   el: '#app',
   data: {
     open: false,
-    // using checkWorry
-    // enableOpen: false,
+    enableOpen: false,
     checkAns: {
       angry: null,
       pride: null,
       surprise: null,
       worry: null,
     },
-    situation: [
-      {
-        type: 'angry',
-        title: '讓人失望透頂 CP 值極低的下午茶 ♥Destino 你是我的命運餐廳',
-      },
-      {
-        type: 'worry',
-        title: '踩到地雷【Pachamama】團購卷 (怎麼辦?! 還有 4 張卷沒用完)',
-      },
-      {
-        type: 'pride',
-        title: '【福鼎湯包店】有做功課有差，果然好吃 (得意) ！',
-      },
-      {
-        type: 'surprise',
-        title: '不起眼、卻超出想像好吃的餐廳!!',
-      },
-    ],
+    situation: {
+      angry: null,
+      pride: null,
+      surprise: null,
+      worry: null,
+    },
+    situationInfo: situationInfo,
     angryData: angryData,
     worryData: worryData,
     prideData: prideData,
@@ -92,17 +86,41 @@ var app = new Vue({
     },
   },
   methods: {
-    changeOrder: function (idx) {
-      var prevIdx = (idx === 0) ? this.situation.length-1 : idx-1;
-      var t = this.situation[idx];
-      Vue.set(this.situation, idx, this.situation[prevIdx])
-      Vue.set(this.situation, prevIdx, t)
+    changeOrder: function (key) {
+      if (!this.situation[key]) this.situation[key] = 1;
+      else if (this.situation[key] < 4) {
+        this.situation[key] = this.situation[key] + 1
+      } else {
+        this.situation[key] = 1;
+      }
+    },
+    hasDuplicate: function (value) {
+      var count = 0;
+      for (key in this.situation){
+        if (this.situation[key] === value) {
+          count++;
+        }
+      }
+      if(count > 1) return true;
+      else return false;
     },
     toggleOpen: function () {
-      if (this.checkWorry)
+      if (this.enableOpen)
         this.open = !this.open;
       else
         alert('請先填寫頁面中的問題');
+    },
+    validateSituation: function () {
+      this.enableOpen = true;
+      for (key in this.situation){
+        if (this.situation[key] === null) {
+          this.enableOpen = false;
+          return;
+        } else if (this.hasDuplicate(this.situation[key])) {
+          this.enableOpen = false;
+          return;
+        }
+      }
     },
     nextPage: function () {
       if (this.position < 3) {
@@ -127,12 +145,10 @@ var app = new Vue({
       var data = JSON.parse(localStorage.userData);
       data.situation = 'all_in_one'
       data.ans = [].concat(angryData, worryData, prideData, surpriseData);
-
-      var situ = processSituationData(this.situation);
-      for (key in situ) {
+      for (key in this.situation) {
         data.ans.push({
           name: key,
-          choose: situ[key],
+          choose: this.situation[key],
           title: key+" 情境的評價"
         });
       }
@@ -152,19 +168,6 @@ var app = new Vue({
     }
   }
 });
-
-function processSituationData(data) {
-  var ret = {
-    angry: null,
-    pride: null,
-    surprise: null,
-    worry: null,
-  };
-  data.forEach(function (e, i) {
-    ret[e.type] = i+1;
-  });
-  return ret;
-}
 
 function uploadAns(data, success, fail){
   $.ajax({
